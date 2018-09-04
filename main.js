36c6711fa4e9a6ab95345fb3e7561f39f5357e69
@ivelton2018 <ivelton2018@gmail.com>
2018-09-04 19:30:18 -0400

    chain validation check

--- main.js
+++ main.js
@@ -32,11 +32,36 @@ class Blockchain{
     newBlock.hash = newBlock.calculateHash()
     this.tinychain.push(newBlock) 
   }
+
+  isChainValid(){
+    // checks the whole chain
+    for(let i=1; i<this.tinychain.length; i++){
+      const currentBlock = this.tinychain[i];
+      const previousBlock = this.tinychain[i-1];
+      // hash still valid ?
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+      // current block points to the right block?
+      if(currentBlock.previousHash !== previousBlock.hash) {
+        return false;        
+      }
+    }
+    // chain is valid
+    return true;
+  }
 }
 
 // instantiate tinychain
 let tinyCoin = new Blockchain()
 tinyCoin.addBlock(new Block(1, "01/02/2018", {amount:5}))
 tinyCoin.addBlock(new Block(2, "02/02/2018", {amount:10}))
-console.log(JSON.stringify(tinyCoin, null, 4))
+console.log("is chain valid ? " + tinyCoin.isChainValid())
+// tampering block
+tinyCoin.tinychain[1].data = {amount:50}
+tinyCoin.tinychain[1].hash = tinyCoin.tinychain[1].calculateHash()
+console.log("is chain valid ? " + tinyCoin.isChainValid())
+
+
+//console.log(JSON.stringify(tinyCoin, null, 4))
 
