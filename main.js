c9b4ccbf95106d46434b8e32d2e3ceffb5c22686
@ivelton2018 <ivelton2018@gmail.com>
2018-09-04 18:37:30 -0400

    block class

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+const SHA256 = require('crypto-js/sha256')
+
 class block {
-  constructor(index, timestamp, data, previousHash){
-    
+  constructor(index, timestamp, data, previousHash=''){
+    this.index = index
+    this.timestamp = timestamp
+    this.data = data
+    this.previousHash = previousHash
+    this.hash = this.calculateHash()
+  }
+
+  calculateHash(){
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringfy(this.data)).toString()
   }
 }
